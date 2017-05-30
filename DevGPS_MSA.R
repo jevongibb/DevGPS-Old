72e203244749a999fbea0dbdf153c8d6e64e65fb
Jevon <jevongibb@gmail.com>
2017-05-29 21:27:35 -0400

    Fixed data suppression issues

--- DevGPS_MSA.R
+++ DevGPS_MSA.R
@@ -9,21 +9,38 @@ library(tidyr)
 setwd("C:/Users/Jevon/Desktop/DevGPS/Data")
 
 # read input data
-Initial_data <- read.table("cbp05msa.txt", header=TRUE, sep=",", stringsAsFactors=FALSE)
-# remember to load different Cities/MSAs by different Census periods
-Regions <- read.table("Cities_2003.csv", header=TRUE, sep=",", quote="\"", stringsAsFactors=FALSE)
+Initial_data <- read.table("cbp14msa.txt", header=TRUE, sep=",", stringsAsFactors=FALSE)
+# remember to load different Cities/MSAs by different Census periods ('03, '07, '12)
+Regions <- read.table("Cities_2012.csv", header=TRUE, sep=",", quote="\"", stringsAsFactors=FALSE)
 Industries <- read.table("NAICS.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 GDP <- read.table("2014_GDP_MSA.csv", header=TRUE, sep=",", stringsAsFactors=FALSE, check.names = FALSE)
 
-# summarise input data by products, markets, and quantifying data
-Data <- Initial_data %>% group_by(msa, naics) %>% summarise(emp=sum(emp))
+#Pull relevant data from initial data
+Data <- data.frame("Region" = Initial_data[,1], 
+                  "Industry" = Initial_data[,2], 
+                  "Flag" = Initial_data[,3],
+                  "Quantity" = Initial_data[,5]) 
 
-# rename columns on Data
-colnames(Data) <- c("Region", "Industry", "Quantity")
-
-## Remove all codes with less than 6 digits
+# Remove all NAICS codes with less than 6 digits
 Data <- Data[ grep("/", Data$Industry, invert = TRUE),]
 Data <- Data[ grep("-", Data$Industry, invert = TRUE),]
+
+# Insert employment quantities for Region/NAICS combinations with Data Suppression Flags
+Data$Quantity[with(Data, Flag == "A")] <- 10
+Data$Quantity[with(Data, Flag == "B")] <- 60
+Data$Quantity[with(Data, Flag == "C")] <- 175
+Data$Quantity[with(Data, Flag == "E")] <- 375
+Data$Quantity[with(Data, Flag == "F")] <- 750
+Data$Quantity[with(Data, Flag == "G")] <- 1750
+Data$Quantity[with(Data, Flag == "H")] <- 3750
+Data$Quantity[with(Data, Flag == "I")] <- 7500
+Data$Quantity[with(Data, Flag == "J")] <- 17500
+Data$Quantity[with(Data, Flag == "K")] <- 37500
+Data$Quantity[with(Data, Flag == "L")] <- 75000
+Data$Quantity[with(Data, Flag == "M")] <- 150000
+
+# Remove Flag column from Data
+Data$Flag <- NULL
 
 # summarise trade value by country and product separately
 DataRegionTotal <- Data %>% group_by(Region) %>% summarise(RegionTotal=sum(Quantity))
@@ -96,12 +113,18 @@ INS <- data.frame(Industry=as.integer(colnames(Template[2:ncol(Template)])), str
 INS$INS <- as.numeric(KP_final)
 INS <- left_join(INS, Industries, by = c("Industry" = "NAICS"))
 
-# Create RNS for export
+# Create RNS, RA, and Employees for export
 RNS <- RNS %>% left_join(Regions, by = c("Region" = "Code"))
-write.csv(RNS, "RNS_MSA_2005.csv", row.names = FALSE)
+RA <- spread(Calc1, Industry, RA, fill=0)
+Emp <- spread(Data, Industry, Quantity, fill=0)
 
+# Export Data to CSV
+write.csv(RNS, "RNS_MSA_2014.csv", row.names = FALSE)
+write.csv(RA, "RA_MSA_2014.csv", row.names = FALSE)
+write.csv(Emp, "Emp_MSA_2014.csv", row.names = FALSE)
 
-
+### END OF DATA GENERATION
+### FOLLOWING SECTION IS ONLY FOR ANALYSIS
 
 
 
@@ -115,12 +138,12 @@ RNS2$KC0 <- KC0$Count
 RNS2$Emp <- Emp$Count
 gdp_reg_model <- lm(Log_GDP ~ RNS + Population + KC0 + Emp, data=RNS2)
 summary(gdp_reg_model)
-percapita_reg_model <- lm(Log_PerCapita ~ RNS + Population + KC0 + Emp, data=RNS2)
+percapita_reg_model <- lm(Log_PerCapita ~ RNS, data=RNS2)
 summary(percapita_reg_model)
 
 library(ggplot2)
 library(plotly)
 
 colnames(RNS2)[3] <- "Name"
-mygraph <- ggplot(RNS2, aes(x = RNS, y = Log_PerCapita, label = Name)) + geom_point() + geom_smooth(method = 'lm') + labs(x="Economic Network Rating", y="Log GDP Per Capita")
+mygraph <- ggplot(RNS2, aes(x = RNS, y = Log_PerCapita, label = Name)) + geom_point() + geom_smooth(method = 'lm') + labs(x="Economic Network Rating", y="Log GDP Per Capita") + xlim(-1,3.6) + ylim(4.3,5.2)
 ggplotly(mygraph)
